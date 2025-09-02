b95ae819e21d522d9069c156d94f2570bdbdaf2e
EVIL67913 <nayticgill827@gmail.com>
2025-09-02 15:58:29 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~gj0QXDYb#wq3oGIct900V7irju5oib6KL5aMnDOjENKC0WDB7qRQ",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923141520266", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Pakistan/nowshera" //put your country timeZone....leave blank if u don't know.
 };
